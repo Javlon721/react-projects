93864049db9baf020f12280a371c76ae0c2d979a
Javlon Talipov <javlon.talipov13@mail.ru>
2022-11-02 22:43:46 +0500

    First commit in HangMan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 
 function App() {
 	//================================================================================================================================
-	return <div className="wrapper"> </div>;
+	return (
+		<div className="wrapper">
+			<h1 className="title">Hello to the HangMan Game</h1>
+			<div className="hangman">
+				<div className="hangman__status">Win Lose</div>
+				<HangManDrawing />
+				<HangmanWord />
+				<HangmanKeyBoard />
+			</div>
+		</div>
+	);
 }
 
 export default App;
