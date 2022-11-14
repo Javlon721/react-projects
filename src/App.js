afff3fec35ccdbd2a5869991e4b692d5ea8d6fec
Javlon Talipov <javlon.talipov13@mail.ru>
2022-11-14 18:08:06 +0500

    changer ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Block } from "./Block";
 
 function App() {
-	//================================================================================================================================
-	return <div className="wrapper"> </div>;
+	const ratesRef = useRef({});
+	const [fromCurrency, setFromCurrency] = useState("RUB");
+	const [toCurrency, setToCurrency] = useState("USD");
+	const [fromPrice, setFromPrice] = useState(0);
+	const [toPrice, setToPrice] = useState(1);
+
+	useEffect(() => {
+		fetch("https://api.exchangerate.host/latest")
+			.then((response) => response.json())
+			.then((res) => {
+				ratesRef.current = res.rates;
+				
+				onChangeToPrice(1);
+			});
+	}, []);
+
+	const onChangeFromPrice = (value) => {
+		const price =
+			(value / ratesRef.current[fromCurrency]) * ratesRef.current[toCurrency];
+		setFromPrice(value);
+		setToPrice(Math.round(price));
+	};
+
+	const onChangeToPrice = (value) => {
+		const price =
+			(value / ratesRef.current[toCurrency]) * ratesRef.current[fromCurrency];
+		setToPrice(value);
+		setFromPrice(Math.round(price));
+	};
+
+	useEffect(() => {
+		onChangeFromPrice(fromPrice);
+	}, [fromCurrency]);
+
+	useEffect(() => {
+		onChangeToPrice(toPrice);
+	}, [toCurrency]);
+
+	return (
+		<div className="wrapper">
+			<Block
+				value={fromPrice}
+				currency={fromCurrency}
+				onChangeCurrency={setFromCurrency}
+				onChangeValue={onChangeFromPrice}
+			/>
+			<Block
+				value={toPrice}
+				currency={toCurrency}
+				onChangeCurrency={setToCurrency}
+				onChangeValue={onChangeToPrice}
+			/>
+		</div>
+	);
 }
 
 export default App;
